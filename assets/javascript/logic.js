84aa752de62e4a4d7424e481b083c8e644bfa8ac
Brendan Ha <ha.brendan07@gmail.com>
2017-08-05 13:34:47 -0700

    added files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 
+// Deal of the day to be displayed on page load
+var startPanelImages = [];
+
+var queryURL = "http://api.sqoot.com/v2/deals/?online=true&per_page=4";
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+		headers: {
+      		"Authorization" : "api_key xlagn7" 
+      }
+	}).done(function(response){
+		console.log(response);
+
+		for(var i=0; i<response.deals.length; i++) {
+			var dealPic = $("<img>");
+			dealPic.attr("src", response.deals[i].deal.image_url);
+			var shortTitle = $("<h2>").html(response.deals[i].deal.short_title);
+			
+			$("#first").append(dealPic);
+			$("#firstText").append(shortTitle);
+			
+
+
+		}
+	})
+
+
+
+
+
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
 });
